e9d3e28cd9ae4166d6d644bc7436f0ef965e6e8b
tle877 <60452318+tle877@users.noreply.github.com>
2023-03-29 22:16:57 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+
+let isMain = true;
+
+function renderGame(){
+  document.body.classList.add("questionTriggeredfun", "fun")
+  isMain=false;
+  let dice = Math.floor(Math.random()*5+1)
+  let defaultHtml=`<h1 id="title">🤖 PARTY GAMES 🤖</h1>
+  <h4 id="activity">Click the button to play</h4>
+  <button id="get-activity"></button>`
+  let contentHtml=''
+  switch (dice){
+    case 1:
+      fetch("https://api.truthordarebot.xyz/v1/truth")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        contentHtml=`
+          <h1 id="title">You are assigned to: </h1>
+          <h1 class="topic">✌ TRUTH ✌ </h1> 
+          <h2>Your questions is:</h2>
+          <p>${data.question}</p> 
+          <h4 id="activity">Click to go back to main page</h4>
+          `
+        document.getElementById("main").innerHTML=contentHtml
+      })
+      break;
+    case 2:
+      fetch("https://api.truthordarebot.xyz/api/dare")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        contentHtml=`
+          <h1 id="title">You are assigned to </h1>
+          <h1 class="topic">😂 DARE 😂</h1>
+          <h2>Your questions is:</h2>
+          <p>${data.question}</p> 
+          <h4 id="activity">Click to go back to main page</h4>
+          `
+        document.getElementById("main").innerHTML=contentHtml
+      })
+      break;
+    case 3:
+      fetch("https://api.truthordarebot.xyz/api/wyr")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        contentHtml=`
+          <h1 id="title">You are assigned to: </h1>
+          <h1 class="topic">👏 WOULD YOU RATHER 👏</h1>
+          <h2>Your questions is:</h2>
+          <p>${data.question} Explain Why.</p> 
+          <h4 id="activity">Click to go back to main page</h4>
+          `
+        document.getElementById("main").innerHTML=contentHtml
+      })
+      break;
+      case 4:
+      fetch("https://api.truthordarebot.xyz/api/paranoia")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        contentHtml=`
+          <h1 id="title">You are assigned to:</h1>
+          <h1 class="topic">🙏 PARANOIA QUESTION 🙏</h1>
+          <h2>Your questions is:</h2>
+          <p>${data.question} Explain Why.</p> 
+          <h4 id="activity">Click to go back to main page</h4>
+          `
+        document.getElementById("main").innerHTML=contentHtml
+      })
+      break;
+      case 5:
+      fetch("https://api.truthordarebot.xyz/api/nhie")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        contentHtml=`
+          <h1 id="title">Eveyone must join: </h1>
+          <h1 class="topic"> 🍾 NEVER HAVE I EVER 🍾</h1>
+          <h2>If u ever have done it before , lets' drink </h2>
+          <p>${data.question} Explain Why.</p> 
+          <h4 id="activity">Click to go back to main page</h4>
+          `
+        document.getElementById("main").innerHTML=contentHtml
+      })
+      break;
+    
+    
+  }
+}
+
+function renderMain(){
+  document.getElementById("main").innerHTML=`
+    <h1 id="title">🤖 PARTY GAMES 🤖</h1>
+    <img src ="funnyBeer.webp"
+    <h3 id="activity">Click the button to play</h4>`
+  isMain=true
+  document.body.classList.remove("questionTriggered", "fun")
+}
+
+document.getElementById("get-activity").addEventListener("click", function(){
+  isMain ? renderGame() : renderMain()
+})
+
+renderMain()
+ 
